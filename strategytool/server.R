9ebb180357ac707c1c9e658a9efdcf0bf7c0fc4e
Millson D (David) <David.Millson@gov.scot>
2025-03-21 17:16:08 +0000

    Feature: Updated to strategy tool functionality in the app

--- strategytool/server.R
+++ strategytool/server.R
@@ -1,26 +1,45 @@
 load_app = function(filepath) {
-  nzst_shiny(network = filepath,
-             title = "System",
-             colours = nzst_colours(base_palette = palette_sg),
-             map_type = "System",
+  nodes = readxl::read_excel(filepath, sheet = "Actions") %>% 
+    dplyr::transmute(data_version = Version,
+                     data_name = "Strategy",
+                     System = "Strategy", 
+                     Variable.ID = ID,
+                     Section.on.Systems.Map = Source,
+                     Variable.Name = Action,
+                     Description = Description,
+                     Source.organisation = Source %>% stringr::str_c(Clause %>% as.character() %>% tidyr::replace_na(""), sep = " "),
+                     Tags = Source,
+                     x = x, y = y)
+  
+  edges = readxl::read_excel(filepath, sheet = "Impacts") %>% 
+    dplyr::transmute(data_name = nodes$data_name[1],
+                     Variable.ID.A = Action_ID,
+                     Variable.ID.B = Impact_ID,
+                     Relationship = Relationship,
+                     Relationship.Strength = Relationship_strength)
+  
+  nzst_shiny(network = list(nodes = nodes, edges = edges),
+             title = "Strategy",
+             colours = nzst_colours(base_palette = palette_sg_dark),
+             map_type = "Strategy",
+             map_type_plural = "Strategies",
              depth_limit = 5, 
-             show_logo = FALSE)
+             show_logo = FALSE,
+             marker_size = 8, text_length = 300, node_font_size = 96, circ_radius = 100)
 }
 
 shinyServer(function( input, output, session) {
   output$map = renderNZST({}) # empty map to serve as a placeholder until the data is selected
   
-  # load from a file
   observeEvent(input$network_file, {
     output$map = renderNZST({
       load_app(input$network_file$datapath)
     })
   })
   
-  # load the example data
   observeEvent(input$show_example, {
     output$map = renderNZST({
-      load_app("www/dummy_system.xlsx")
+      load_app("www/dummy_strategy.xlsx")
     })
   })
 })
